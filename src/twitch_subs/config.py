8c3db523dce0c3e397293ad9e5099e23d197ec81
nikmosi <i@nikmosi.ru>
2025-08-22 12:34:44 +0700

    refactor(config): add default

--- src/twitch_subs/config.py
+++ src/twitch_subs/config.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    twitch_client_id: str
-    twitch_client_secret: str
-    telegram_bot_token: str
-    telegram_chat_id: str
+    twitch_client_id: str = Field(default=...)
+    twitch_client_secret: str = Field(default=...)
+    telegram_bot_token: str = Field(default=...)
+    telegram_chat_id: str = Field(default=...)
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
