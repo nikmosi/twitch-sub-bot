c4ecf11d09c0c3c9771a4f44e00183ce39ad0a2c
nikmosi <i@nikmosi.ru>
2025-08-25 23:56:27 +0700

    fix(logings): remove error on db init

--- src/twitch_subs/infrastructure/logings.py
+++ src/twitch_subs/infrastructure/logings.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from twitch_subs.application.logins import LoginsProvider
 
 from ..domain.ports import WatchlistRepository
-from .error import WatchListIsEmpty
 
 
 class WatchListLoginProvider(LoginsProvider):
@@ -12,6 +11,4 @@ class WatchListLoginProvider(LoginsProvider):
 
     def get(self) -> list[str]:
         logins = self.repo.list()
-        if not logins:
-            raise WatchListIsEmpty()
         return logins
