32fbb6315b77930eed04d28a5836958590fca13c
nikmosi <i@nikmosi.ru>
2025-10-19 16:42:21 +0700

    feat: move to pydantic models

--- src/twitch_subs/domain/models.py
+++ src/twitch_subs/domain/models.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
+from dataclasses import field
 from datetime import datetime, timezone
 from enum import Enum
+
+from pydantic.dataclasses import dataclass
 
 
 class BroadcasterType(str, Enum):
