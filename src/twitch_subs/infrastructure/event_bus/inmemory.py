50c5488b93ddd0f5f941e288c2f128db1a86c644
nikmosi <i@nikmosi.ru>
2025-10-22 23:37:43 +0700

    feat(InMemoryEventBus): added duplicate event control

--- src/twitch_subs/infrastructure/event_bus/inmemory.py
+++ src/twitch_subs/infrastructure/event_bus/inmemory.py
@@ -2,9 +2,11 @@
 
 from __future__ import annotations
 
-from collections import defaultdict
+from collections import defaultdict, deque
 from dataclasses import dataclass, field
-from typing import Any, DefaultDict, TypeVar, cast
+from typing import Any, DefaultDict, Deque, TypeVar
+
+from loguru import logger
 
 from twitch_subs.application.ports import EventBus, Handler
 from twitch_subs.domain.events import DomainEvent
@@ -16,16 +18,25 @@ T = TypeVar("T", bound=DomainEvent)
 class InMemoryEventBus(EventBus):
     """Simple event bus dispatching events to in-process handlers."""
 
+    idempotency_queue_maxlen = 100
+
     _handlers: DefaultDict[type[DomainEvent], list[Handler[Any]]] = field(
         default_factory=lambda: defaultdict(list)
     )
 
+    _idempotency_queue: Deque[DomainEvent] = deque(maxlen=idempotency_queue_maxlen)
+
     async def publish(self, *events: DomainEvent) -> None:
         for event in events:
+            if event in self._idempotency_queue:
+                logger.warning(f"Get duplicated event {event}.")
+                continue
+
+            self._idempotency_queue.append(event)
             for event_type, handlers in self._handlers.items():
                 if isinstance(event, event_type):
                     for handler in handlers:
-                        await handler(cast(Any, event))
+                        await handler(event)
 
     def subscribe(self, event_type: type[T], handler: Handler[T]) -> None:
         self._handlers[event_type].append(handler)
